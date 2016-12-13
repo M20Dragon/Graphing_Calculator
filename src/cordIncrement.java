32dead19622a76615af58f8afdafa0df9661f07f
M20Dragon <thguler@gmail.com>
2016-12-13 16:28:10 -0600

    First Commit

---
+++ src/cordIncrement.java
@@ -0,0 +1,16 @@
+import javafx.scene.shape.Line;
+
+/**
+ * Created by timurguler on 11/6/16.
+ */
+public class cordIncrement extends Line
+{
+    public cordIncrement()
+    {
+
+    }
+    public void markIncrement()
+    {
+
+    }
+}
---
+++ src/lineMaker.java
@@ -0,0 +1,40 @@
+import javafx.scene.shape.Line;
+
+import static javafx.scene.paint.Color.BLUE;
+
+/**
+ * Created by timurguler on 10/30/16.
+ */
+public class lineMaker extends Line
+{
+    private double xStart = 0;
+    private double yStart = 0;
+    private double xEnd = 0;
+    private double yEnd = 0;
+    private double zoom = 0;
+
+    public lineMaker(double xStart, double yStart, double xEnd, double yEnd, double zoom)
+    {
+        super();
+        this.xStart = xStart;
+        this.yStart = yStart;
+        this.xEnd = xEnd;
+        this.yEnd = yEnd;
+        this.zoom = zoom;
+        makeLine();
+    }
+    public void makeLine()
+    {
+        lineMaker.super.setStartX(xStart);
+        lineMaker.super.setStartY(yStart);
+        lineMaker.super.setEndX(xEnd);
+        lineMaker.super.setEndY(yEnd);
+        lineMaker.super.setStroke(BLUE);
+        lineMaker.super.setStrokeWidth(2.5);
+        //sets the lines in the right area
+        lineMaker.super.setRotate(180);
+        lineMaker.super.setTranslateX(((xEnd - 250) * -1) * (Math.abs(zoom)));
+        lineMaker.super.setTranslateY((yEnd - (yEnd * 2)) * (Math.abs(zoom)/**5*/));
+    }
+
+}
